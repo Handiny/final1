03762ebd2ad40f862681e839278743e2563ed2eb
Chaima2703 <74559699+Chaima2703@users.noreply.github.com>
2021-05-21 20:38:31 +0100

    Add files via upload

--- Version2-main/mvc/view/contact/viewCreateContact.php
+++ Version2-main/mvc/view/contact/viewCreateContact.php
@@ -20,10 +20,10 @@
         </div>
     
         <!--Box2-->
-        <div class="box">
-            <div class="icon"><i class="fas fa-phone-square-alt"></i></div>
-            <div class="text">
-                 <h3>Phone</h3>
+        <div class="box" >
+            <div class="icon" ><i class="fas fa-phone-square-alt"></i></div>
+            <div class="text" >
+                 <h3 class="succes">Phone</h3>
                  <p>+21625847932</p>
             </div>
         </div>
@@ -31,38 +31,64 @@
         <div class="box">
             <div class="icon"><i class="fas fa-envelope-square"></i></div>
             <div class="text">
-                 <h3>Email</h3>
+                 <h3 >Email</h3>
                  <p>Handiny@yahoo.com</p>
             </div>
         </div>
       </div>
     
         <!--Formulaire de contact-->
-        <div class="contactForm">
-        <form>
-            <h2>Send Message</h2>
-            <div class="inputBox">
-                <span>Full Name</span>
-                <input type="text" name="name" required>
-                 
-            </div>
-            <div class="inputBox">
-                <span>Email</span>
-                <input type="email" name="email" required>
-                 
-            </div>
-            <div class="inputBox">
-                <span>Type your Message</span>
-                 <br>
-                <textarea required></textarea>
-               
-                 
-            </div>
-            <div class="inputBox">
-                <input type="submit" name="submit" value="Send" required>
-                
-            </div>
-        </form>
+        <div class="">
+        <form action="index.php?controller=contact&action=created" method="POST">
+                        <div class="card border-primary rounded-0">
+                            <div class="card-header p-0">
+                                <div class="bg-info text-white text-center py-2" id="bodyy">
+                                    <h3><i class="fa fa-envelope "></i> Contactez-nous</h3>
+                                
+                                </div>
+                            </div>
+                            <div class="card-body p-3">
+
+                                <!--Body-->
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-user text-info"></i></div>
+                                        </div>
+                                        <input type="text" class="form-control"  name="nom_prenom" placeholder="Nom et Prénom" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-envelope text-info"></i></div>
+                                        </div>
+                                        <input type="text" class="form-control"  name="email" placeholder="E-mail" required>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-comment text-info"></i></div>
+                                        </div>
+                                        <textarea class="form-control" placeholder="Message" name="message" required></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="text-center">
+                                    <input type="submit" value="Envoyer" class="">
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
+                   
+
+
+                </div>
+	</div>
+</div>
         </div>
 
 
